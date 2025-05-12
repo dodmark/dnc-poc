0b908bbb2eb0f384487dadc53a0b2e2f1ae6ab94
David Odmark <davidodmark@Dio.local>
2025-05-12 08:53:47 -0500

    replaced README.md and removed profanity (-;

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World, now fuck off';
+    return 'Hello World, now go away';
   }
 }
